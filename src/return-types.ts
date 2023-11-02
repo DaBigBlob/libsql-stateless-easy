0249097be82b3cb1ba40024aa4e1ac028c579378
Person <localboxcrox@gmail.com>
2023-11-02 07:03:01 -0400

    cleanup

--- src/return-types.ts
+++ src/return-types.ts
@@ -1,5 +1,4 @@
 type Ok<T> = { isOk: true, val: T};
-
 type Err<E> = { isOk: false, err: E};
 
 export type Result<T, E> = Ok<T>|Err<E>;
@@ -7,17 +6,6 @@ export type Result<T, E> = Ok<T>|Err<E>;
 export function Ok<T>(val: T): Ok<T> {
     return { isOk: true, val};
 }
-
 export function Err<E>(err: E): Err<E> {
     return { isOk: false, err};
-}
-
-export function getVal<T, E>(result: Result<T, E>) {
-    if (result.isOk) return result.val;
-    return null;
-}
-
-export function getErr<T, E>(result: Result<T, E>) {
-    if (!result.isOk) return result.err;
-    return null;
 }
