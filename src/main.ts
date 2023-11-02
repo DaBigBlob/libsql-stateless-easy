0dd2e26a505ddec6fe0b901e545825dcdcb7c9e9
Person <localboxcrox@gmail.com>
2023-11-02 01:06:38 -0400

    lil bit bootstrapping

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,17 @@
+import { Config, InStatement, ResultSet, TransactionMode } from "./api";
+
 export class createClient {
-    constructor(s: {
-        /** The database URL.
-         *
-         * The client supports `libsql:`, `http:`/`https:`, `ws:`/`wss:` and `file:` URL. For more infomation,
-         * please refer to the project README:
-         *
-         * https://github.com/libsql/libsql-client-ts#supported-urls
-         */
-        url: string,
-    
-        /** Authentication token for the database. */
-        authToken?: string,
-    
-        /** How to convert SQLite integers to JavaScript values:
-         *
-         * - `"number"` (default): returns SQLite integers as JavaScript `number`-s (double precision floats).
-         * `number` cannot precisely represent integers larger than 2^53-1 in absolute value, so attempting to read
-         * larger integers will throw a `RangeError`.
-         * - `"bigint"`: returns SQLite integers as JavaScript `bigint`-s (arbitrary precision integers). Bigints can
-         * precisely represent all SQLite integers.
-         * - `"string"`: returns SQLite integers as strings.
-         */
-        intMode?: "number" | "bigint" | "string"
-    }) {
-        
+    constructor(config: Config) {
+        if (config) {}; //place holder
+    }
+
+    async execute(stmt: InStatement): Promise<ResultSet> {
+        if (stmt) {}; //place holder
+        return {} as ResultSet; //place holder
+    }
+
+    async batch(stmts: Array<InStatement>, mode: TransactionMode = "deferred"): Promise<Array<ResultSet>> {
+        if (stmts||mode) {}; //place holder
+        return {} as Array<ResultSet>; //place holder
     }
 }
