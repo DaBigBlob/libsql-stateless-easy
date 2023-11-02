917661f3a7e8655fd71f3e0826bae6a5bb70a88d
Person <localboxcrox@gmail.com>
2023-11-02 07:00:41 -0400

    almost done

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { _BatchResponse, _ErrorResponse, _Query } from "./api";
 import { Err, Ok, Result } from "./return-types";
 
+/** Check if the server is compatible with sqld http API v0 */
+export async function checkServerCompat(
+    /** The database URL.
+     *
+     * The client supports `http:`/`https:` URL.
+     * 
+     */
+    url: string
+): Promise<Result<undefined, undefined>> {
+    if ((await fetch(url, {method: 'POST'})).status==401) return Ok(undefined);
+    else return Err(undefined);
+}
+
 export async function executeBatch(
     config: {
         url: string,
@@ -15,17 +28,4 @@ export async function executeBatch(
     });
     if (res.ok) return Ok(await res.json() as _BatchResponse);
     else return Err(await res.json() as _ErrorResponse);
-}
-
-/** Check if the server is compatible with sqld http API v0 */
-export async function checkServerCompat(
-    /** The database URL.
-     *
-     * The client supports `http:`/`https:` URL.
-     * 
-     */
-    url: string
-): Promise<Result<undefined, undefined>> {
-    if ((await fetch(url, {method: 'POST'})).status==401) return Ok(undefined);
-    else return Err(undefined);
 }
