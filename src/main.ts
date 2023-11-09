dd10812263f749b9526f8a43766636d3aef3573e
Person <localboxcrox@gmail.com>
2023-11-09 14:45:45 -0500

    some stuff

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
 export * from './functions';
-
-export { BatchReqStep, BatchStreamResOkData, Config, PipelineReq, PipelineResErr, PipelineResOk, Result, SQLStatement, StatementResOkData, StreamResErr, StreamResErrData } from "./types";
+export * from "./types";
 
