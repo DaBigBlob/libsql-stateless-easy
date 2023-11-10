b09b5bdba1a41fe5fe68ccc56148a1f0b6c0c513
Person <localboxcrox@gmail.com>
2023-11-10 15:58:02 -0500

    stuff

--- src/parsers.ts
+++ src/parsers.ts
@@ -71,7 +71,7 @@ export function libsqlBatchStreamResParser(
     let batchResults: Array<ResultSet> = [];
     for (let j=0;j<res.step_results.length;j++) {
         if (res.step_results[j]) batchResults.push(libsqlStatementResParser(res.step_results[j]!));
-        else throw Error("Batch didn't execute successfully.");
+        else throw Error("Entire batch didn't execute successfully.");
     }
     return batchResults;
 }
--- src/simple_functions.ts
+++ src/simple_functions.ts
@@ -1,1 +1,19 @@
-export {};
+import { libsqlExecute as LIBlibsqlExecute, libsqlBatch as LIBlibsqlBatch, libsqlConfig, libsqlBatchReqStep } from "libsql-stateless";
+import { ResultSet, rawSQLStatement } from "./types";
+import { SQLStatementBuilder } from "./builders";
+import { libsqlBatchStreamResParser, libsqlStatementResParser } from "./parsers";
+
+export async function libsqlExecute(conf: libsqlConfig, stmt: rawSQLStatement): Promise<ResultSet> {
+    const res = await LIBlibsqlExecute(conf, SQLStatementBuilder(stmt));
+    if (res.isOk) return libsqlStatementResParser(res.val);
+    else throw Error(JSON.stringify(res.err));
+}
+
+export async function libsqlBatch(conf: libsqlConfig, steps: Array<rawSQLStatement>): Promise<Array<ResultSet>> {
+    let _steps: Array<libsqlBatchReqStep> = [];
+    for (let i=0;i<steps.length;i++) _steps.push({stmt: SQLStatementBuilder(steps[i])});
+
+    const res = await LIBlibsqlBatch(conf, _steps);
+    if (res.isOk) return libsqlBatchStreamResParser(res.val);
+    else throw Error(JSON.stringify(res.err));
+}
