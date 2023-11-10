3475a426274958e64d841f6d3509853173fcff12
Person <localboxcrox@gmail.com>
2023-11-10 17:09:54 -0500

    stuff

--- src/client.ts
+++ src/client.ts
@@ -2,32 +2,26 @@ import { libsqlConfig } from "libsql-stateless";
 import { rawSQLStatement } from "./types";
 import { libsqlBatch, libsqlExecute, libsqlServerCompatCheck } from "./simple_functions";
 
-export class libsqlClient {
+class libsqlClient {
     private readonly conf: libsqlConfig;
-    private readonly checkServerOnRun: boolean;
 
-    constructor(conf: libsqlConfig, checkServerOnRun: boolean) {
+    constructor(conf: libsqlConfig) {
         this.conf = conf;
-        this.checkServerOnRun = checkServerOnRun;
     }
 
     public async execute(stmt: rawSQLStatement) {
-        if (
-            this.checkServerOnRun &&
-            !(await this.serverOk())
-        ) throw Error("Server is not compatible with `libsql-stateless`");
         return await libsqlExecute(this.conf, stmt);
     }
 
     public async batch(steps: Array<rawSQLStatement>) {
-        if (
-            this.checkServerOnRun &&
-            !(await this.serverOk())
-        ) throw Error("Server is not compatible with `libsql-stateless`");
         return await libsqlBatch(this.conf, steps);
     }
 
     public async serverOk() {
         return await libsqlServerCompatCheck(this.conf);
     }
+}
+
+export function createClient(conf: libsqlConfig) {
+    return new libsqlClient(conf);
 }
