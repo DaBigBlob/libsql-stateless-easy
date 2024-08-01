5de5fa95d5e236dd7a25aaf5704012203e27b923
Person <localboxcrox@gmail.com>
2024-08-01 18:38:58 -0400

    optimizations in b64 sub-library (test)

--- _tests/test_b64.ts
+++ _tests/test_b64.ts
@@ -6,6 +6,12 @@ const teststr = "I need to write my resume. \n\nT-T";
 (() => {
     const a = toUint8Array(teststr);
     const b = Base64.toUint8Array(teststr);
-    if (fromUint8Array(a) == Base64.fromUint8Array(b)) console.log("success");
-    else console.log("we fucked up");
+    if (fromUint8Array(a) == Base64.fromUint8Array(b)) console.log("[GOOD] same encode");
+    else console.log("[BAD] encode: we fucked up");
+
+    const ra = fromUint8Array(a);
+    const rb = Base64.fromUint8Array(b);
+    if (fromUint8Array(a) == Base64.fromUint8Array(b)) console.log("[GOOD] same decode");
+    else console.log("[BAD] decode: we fucked up");
+
 })();
