9a2247ce7f40c4aec4264e35f3bc3cbe56a1fb9d
Person <localboxcrox@gmail.com>
2024-07-31 20:45:35 -0400

    git add tests

--- _tests/test_special_chars.ts
+++ _tests/test_special_chars.ts
@@ -1,9 +1,11 @@
 import { createClient } from "../src/main";
-// import { createClient } from '@libsql/client';
+//import { createClient } from '@libsql/client/web';
 import { skjdgfksg } from "./conf";
 
 (async () => {
-    const client = createClient({url: skjdgfksg.db_url, authToken: skjdgfksg.authToken, disableCriticalChecks: true});
+    const client = createClient({url: skjdgfksg.db_url, authToken: skjdgfksg.authToken,
+         //disableCriticalChecks: true
+    });
     const res2 = await client.execute({
         sql: "insert into contacts (contact_id,first_name,last_name,email,phone) values (?,?,?,?,?);",
         args: [6,"glomm\nangk","feru","moca@doro.co","001"]
@@ -14,7 +16,7 @@ import { skjdgfksg } from "./conf";
         sql: "select * from contacts where contact_id = :kkl",
         args: {kkl: 6}
     });
-    console.log("\n===WITH SPECIAL CHARS START===\n"+JSON.stringify(res4.rows[0])+"\n===WITH SPECIAL CHARS START===\n");
+    console.log("\n===WITH SPECIAL CHARS START===\n"+JSON.stringify(res4.rows[0], null, 4)+"\n===WITH SPECIAL CHARS START===\n");
 
     const res3 = await client.execute({
         sql: "delete from contacts where contact_id = :kkl",
