ba124ff90964587418e6d61fc0e4d6e0855031f6
Person <localboxcrox@gmail.com>
2024-08-31 15:09:31 -0400

    add examples for custom fetch

---
+++ examples/custom_fetch/generic.ts
@@ -0,0 +1,21 @@
+//A GENERIC BOILERPLATE FOR IMPLEMENTING CUSTOM FETCH
+import { createClient, libsqlFetchLike } from 'libsql-stateless-easy';
+import { conf } from './conf';
+
+(async () => {
+    const client = createClient({
+        url: conf.db_url,
+        authToken: conf.authToken,
+        fetch: async (...args: Parameters<libsqlFetchLike>): ReturnType<libsqlFetchLike> => {
+                //implement your own fetch here (look at libsql_isomorphic_fetch.ts for concrete example)
+                /** NOTE:
+                 * args[0] is the url string
+                 * args[1] is the request init
+                 */
+            );
+        }
+    });
+
+    const res = await client.execute("select * from contacts;", []);
+    console.log(res);
+})();
