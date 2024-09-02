567e7fa81583788781765a088942edac5b7bdfb7
Person <localboxcrox@gmail.com>
2024-09-02 08:51:39 -0400

    lil example update

---
+++ examples/custom_fetch/undici.ts
@@ -0,0 +1,18 @@
+//@ts-nocheck
+//USING UNDICI
+import { fetch as undici_fetch } from 'undici';
+import { createClient, libsqlFetchLike } from 'libsql-stateless-easy';
+import { conf } from './conf';
+
+(async () => {
+    const client = createClient({
+        url: conf.db_url,
+        authToken: conf.authToken,
+        // as easy as that
+        fetch: undici_fetch
+    });
+
+    const res = await client.execute("select * from contacts;");
+    console.log(res);
+})();
+
