24b611ec1ffd1a66677ebef8924d90c52f32fe3d
Person <localboxcrox@gmail.com>
2024-09-11 13:25:19 -0400

    add @libsql/core's lisence to official_api.ts

--- src/commons.ts
+++ src/commons.ts
@@ -10,7 +10,7 @@ import {
     type TransactionMode,
     type InArgs as rawSQLArgs,
     LibsqlError
-} from './official_api_1726012615447.js';
+} from './official_api_1726075384172.js';
 
 export type rawSQL = string;
 export type rawSQLStatement = {
