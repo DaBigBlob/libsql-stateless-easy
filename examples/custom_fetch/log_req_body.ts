4287d0ee2a769f9e8b6b8cc14f007191b1b1aed9
Person <localboxcrox@gmail.com>
2024-09-12 01:59:26 -0400

    add new example

---
+++ examples/custom_fetch/log_req_body.ts
@@ -0,0 +1,20 @@
+//@ts-nocheck
+//USING UNDICI
+import { createClient, libsqlFetchLike } from 'libsql-stateless-easy';
+import { conf } from './conf';
+
+(async () => {
+    const client = createClient({
+        url: conf.db_url,
+        authToken: conf.authToken,
+        // as easy as that
+        fetch: async (...args: Parameters<libsqlFetchLike>): ReturnType<libsqlFetchLike> => {
+            console.log(args[1]?.body);
+            return await globalThis.fetch(...args);
+        }
+    });
+
+    const res = await client.execute("select * from contacts;");
+    console.log(res);
+})();
+
