ad899beb7817b1981ac6ea80cf4ede95866fc308
Person <localboxcrox@gmail.com>
2024-09-12 02:02:17 -0400

    add new example

--- examples/custom_fetch/log_req_body.ts
+++ examples/custom_fetch/log_req_body.ts
@@ -7,7 +7,6 @@ import { conf } from './conf';
     const client = createClient({
         url: conf.db_url,
         authToken: conf.authToken,
-        // as easy as that
         fetch: async (...args: Parameters<libsqlFetchLike>): ReturnType<libsqlFetchLike> => {
             console.log(args[1]?.body);
             return await globalThis.fetch(...args);
